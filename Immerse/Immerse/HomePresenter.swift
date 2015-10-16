c969fb706968e8685763ce83a78e6140a42b622b
James Tan <jamesktan@gmail.com>
2015-10-15 23:07:19 -0700

    Stubbing in files and putting together structure

---
+++ Immerse/Immerse/HomePresenter.swift
@@ -0,0 +1,13 @@
+//
+//  HomePresenter.swift
+//  Immerse
+//
+//  Created by James Tan on 10/15/15.
+//  Copyright © 2015 Immerse. All rights reserved.
+//
+
+import UIKit
+
+class HomePresenter: NSObject {
+
+}
