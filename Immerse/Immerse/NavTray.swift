4455b29b1e4147cc463cfb606ab27cb6c6afe0e1
James Tan <jamesktan@gmail.com>
2015-10-16 19:48:55 -0700

    Styled the navigation and working on links

--- Immerse/Immerse/NavTray.swift
+++ Immerse/Immerse/NavTray.swift
@@ -20,14 +20,6 @@ class NavTray: UITableViewController {
   }
 
   // MARK: - Table view data source
-
-//  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-//    return 0
-//  }
-//
-//  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//    return 0
-//  }
   
   override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
     tableView.deselectRowAtIndexPath(indexPath, animated: true)
