7e17ec9faa955bb2eb15b9e59b4e83fb99013472
James Tan <jamesktan@gmail.com>
2016-01-09 17:16:15 -0800

    Created the RefCell

--- Immerse/Immerse/RefCell.swift
+++ Immerse/Immerse/RefCell.swift
@@ -10,15 +10,18 @@ import UIKit
 
 class RefCell: UITableViewCell {
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
+  @IBOutlet weak var numberOfReferences: UIImageView!
+  @IBOutlet weak var selectionButton: UIImageView!
+  @IBOutlet weak var sourceWritingName: UILabel!
+  @IBOutlet weak var referenceWritingName: UILabel!
+  
+  override func awakeFromNib() {
+    super.awakeFromNib()
+  }
 
-    override func setSelected(selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+  override func setSelected(selected: Bool, animated: Bool) {
+    super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
-    }
-    
+  }
+  
 }
